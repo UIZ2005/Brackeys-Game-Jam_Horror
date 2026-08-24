cd1f91e0927c70d7256968bbd53f902a7bfa0e37
JUANSUAREZ97 <juan_esteban.suarez@uao.edu.co>
2026-08-24 18:43:05 -0500

    PJ1
    Agregue el sprite sheet del PJ con el que vamos a jugar con sus frames de cada animacion, idle(que se puede usar la misma para ir hacia abajo), arriba, arriba y derecha al mismo tiempo, abajo y derecha al mismo tiempo, y derecha. Estas ultimas pueden usarse para implementar su sentido contrario.

---
+++ Horror_Game/Assets/suarez/Script/Player.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+public float speed = 5f;
+private Rigidbody2D rb2D;
+private Vector2 movementInput;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        rb2D = GetComponent<Rigidbody2D>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        movementInput.x = Input.GetAxisRaw("Horizontal");
+        movementInput.y = Input.GetAxisRaw("Vertical");
+
+        movementInput=movementInput.normalized;
+
+    }
+
+    private void FixedUpdate()
+    {
+        rb2D.linearVelocity = movementInput * speed;
+    }
+}
